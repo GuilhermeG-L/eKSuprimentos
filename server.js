b423cbfeb79f94af08b4304a4cb7fef233f79167
Guilherme Lima <93679190+GuilhermeG-L@users.noreply.github.com>
2021-11-11 15:35:55 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -23,36 +23,3 @@ console.log ("TESTE");
     });
     
     connection.connect();
-    
-
-// TESTE
-/*
-    var Request = require('tedious').Request  
-    var TYPES = require('tedious').TYPES;  
-  
-    function executeStatement1() {  
-        request = new Request("INSERT SalesLT.Product (Name, ProductNumber, StandardCost, ListPrice, SellStartDate) OUTPUT INSERTED.ProductID VALUES (@Name, @Number, @Cost, @Price, CURRENT_TIMESTAMP);", function(err) {  
-         if (err) {  
-            console.log(err);}  
-        });  
-        request.addParameter('Name', TYPES.NVarChar,'SQL Server Express 2014');  
-        request.addParameter('Number', TYPES.NVarChar , 'SQLEXPRESS2014');  
-        request.addParameter('Cost', TYPES.Int, 11);  
-        request.addParameter('Price', TYPES.Int,11);  
-        request.on('row', function(columns) {  
-            columns.forEach(function(column) {  
-              if (column.value === null) {  
-                console.log('NULL');  
-              } else {  
-                console.log("Product id of inserted item is " + column.value);  
-              }  
-            });  
-        });
-
-        // Close the connection after the final event emitted by the request, after the callback passes
-        request.on("requestCompleted", function (rowCount, more) {
-            connection.close();
-        });
-        connection.execSql(request);  
-    }
-*/
