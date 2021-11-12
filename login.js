c4feebc720122d731edd5e7ede7e29d40af8bc9c
Guilherme Lima <93679190+GuilhermeG-L@users.noreply.github.com>
2021-11-12 13:16:08 -0300

    Add files via upload

--- login.js
+++ login.js
@@ -1,21 +1,10 @@
+let user = document.cookie;
 
-
-btnVendedor.addEventListener('click', function () {
-    user = 'Vendedor';
-})
-
-btnProdutor.addEventListener('click', function () {
-    user = 'Produtor';
-})
-
-btnAdministrador.addEventListener('click', function () {
-    user = 'Administrador';
-})
-
-btnAcessar.addEventListener('click', function () {
+console.log(user);
+document.getElementById("btnAcessar").addEventListener('click', function () {
     switch(true) {
-        case(user = 'Vendedor'): aTest.href="../Telas Vendedor/gerenciarPedidos.html"; break;
-        case(user = 'Produtor'): aTest.href="../Telas Produtor/gerenciarOrdens.html"; break;
-        case(user = 'Administrador'): aTest.href="../Telas Adm/gerenciarOrdens.html"; break;
+        case(user = 'Vendedor'): aLogin.href="../Telas Vendedor/gerenciarPedidos.html"; break;
+        case(user = 'Produtor'): aLogin.href="../Telas Produtor/gerenciarOrdens.html"; break;
+        case(user = 'Administrador'): aLogin.href="../Telas Adm/gerenciarOrdens.html"; break;
     }
 })
