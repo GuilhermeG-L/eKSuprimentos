cfd55dee2c44c456a083c0d194c561dced667a84
Guilherme Lima <93679190+GuilhermeG-L@users.noreply.github.com>
2021-11-14 18:08:05 -0300

    Listagem feita (Exceto Ordem)/Servermodificado

---
+++ listaPedido.js
@@ -0,0 +1,64 @@
+const { Connection, Request } = require("tedious");
+
+  // Configuração de conexão DB.
+  const config = {
+    authentication: {
+      options: {
+        userName: "sqlserver",
+        password: "Proj@MSsql15"
+      },
+      type: "default"
+    },
+    server: "34.151.252.122",
+    options: {
+      database: "eKSuprimentos",
+      encrypt: true
+    }
+  };
+
+  // Query Azure SQL: Busca Produto.
+  const connection = new Connection(config);
+
+  // Tentativa de conexão.
+  connection.on("connect", err => {
+    if (err) {
+      console.error(err.message);
+    } else {queryDatabase();}
+  });
+
+  // Conexão do DB.
+  connection.connect();
+
+  // Função de criação de Query.
+  function queryDatabase() {
+    console.log("Lendo dados da tabela...");
+
+    const request = new Request(
+      `SELECT TOP 5 p.CodPedido, c.Nome, p.PreçoTotal, Data_entrega
+      FROM dbo.Pedido p
+      Inner Join dbo.Cliente c ON p.CodCliente = c.CodCliente
+      Order By Data_entrega`,
+      (err, rowCount) => {
+        if (err) {
+          console.error(err.message);
+        }
+        else {
+          console.log(`${rowCount} linha(s) retornadas`);
+        }
+      }
+    );
+    
+    var i = 1;
+
+    // Console.log da query.
+    request.on("row", columns => {
+      columns.forEach(column => {
+        teste = ("%s\t%s", /*column.metadata.colName,*/ column.value);
+        console.log(teste);
+        document.getElementById(`td${i}`).textContent = teste;
+        i++;
+      });
+    });
+
+    connection.execSql(request);
+  }
---
+++ listaProduto.js
@@ -0,0 +1,63 @@
+const { Connection, Request } = require("tedious");
+
+  // Configuração de conexão DB.
+  const config = {
+    authentication: {
+      options: {
+        userName: "sqlserver",
+        password: "Proj@MSsql15"
+      },
+      type: "default"
+    },
+    server: "34.151.252.122",
+    options: {
+      database: "eKSuprimentos",
+      encrypt: true
+    }
+  };
+
+  // Query Azure SQL: Busca Produto.
+  const connection = new Connection(config);
+
+  // Tentativa de conexão.
+  connection.on("connect", err => {
+    if (err) {
+      console.error(err.message);
+    } else {queryDatabase();}
+  });
+
+  // Conexão do DB.
+  connection.connect();
+
+  // Função de criação de Query.
+  function queryDatabase() {
+    console.log("Lendo dados da tabela...");
+
+    const request = new Request(
+      `SELECT DISTINCT TOP 5 CodProduto, Nome, Preço
+      FROM dbo.Produto
+      Order By Nome`,
+      (err, rowCount) => {
+        if (err) {
+          console.error(err.message);
+        }
+        else {
+          console.log(`${rowCount} linha(s) retornadas`);
+        }
+      }
+    );
+    
+    var i = 1;
+
+    // Console.log da query.
+    request.on("row", columns => {
+      columns.forEach(column => {
+        teste = ("%s\t%s", /*column.metadata.colName,*/ column.value);
+        console.log(teste);
+        document.getElementById(`td${i}`).textContent = teste;
+        i++;
+      });
+    });
+
+    connection.execSql(request);
+  }
